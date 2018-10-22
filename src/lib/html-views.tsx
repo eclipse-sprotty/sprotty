4bf54db6e595cadc6101e463d78d3eff88da3702
Miro Spönemann <miro.spoenemann@typefox.io>
2018-10-22 10:00:23 +0200

    [popups] use html jsx factory for HTML VDOMs

--- src/lib/html-views.tsx
+++ src/lib/html-views.tsx
@@ -14,8 +14,8 @@
  * SPDX-License-Identifier: EPL-2.0 OR GPL-2.0 WITH Classpath-exception-2.0
  ********************************************************************************/
 
- /** @jsx svg */
-import { svg } from 'snabbdom-jsx';
+ /** @jsx html */
+import { html } from 'snabbdom-jsx';
 
 import { VNode } from "snabbdom/vnode";
 import { IView, RenderingContext } from "../base/views/view";
