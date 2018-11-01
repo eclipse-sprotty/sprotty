5dfc4eb16e546a9b9696d230aaa64b23eed0ac64
Jan Koehnlein <jan.koehnlein@typefox.io>
2018-11-01 20:41:25 +0100

    Decoration (issue markers) (#14)
    * [decoration] issue markers on elements
    * Applied feedback
    
    Signed-off-by: Jan Koehnlein <jan.koehnlein@typefox.io>

---
+++ src/features/decoration/model.ts
@@ -0,0 +1,47 @@
+/********************************************************************************
+ * Copyright (c) 2018 TypeFox and others.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v. 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0.
+ *
+ * This Source Code may also be made available under the following Secondary
+ * Licenses when the conditions for such availability set forth in the Eclipse
+ * Public License v. 2.0 are satisfied: GNU General Public License, version 2
+ * with the GNU Classpath Exception which is available at
+ * https://www.gnu.org/software/classpath/license.html.
+ *
+ * SPDX-License-Identifier: EPL-2.0 OR GPL-2.0 WITH Classpath-exception-2.0
+ ********************************************************************************/
+
+import { SModelExtension } from "../../base/model/smodel-extension";
+import { hoverFeedbackFeature, popupFeature } from "../hover/model";
+import { SModelElement } from "../../base/model/smodel";
+import { SShapeElement, boundsFeature } from "../bounds/model";
+
+export const decorationFeature = Symbol('decorationFeature');
+
+export interface Decoration extends SModelExtension {
+}
+
+export function isDecoration<T extends SModelElement>(e: T): e is T & Decoration {
+    return e.hasFeature(decorationFeature);
+}
+
+export class SDecoration extends SShapeElement implements Decoration {
+    hasFeature(feature: symbol) {
+        return feature === decorationFeature
+            || feature === boundsFeature
+            || feature === hoverFeedbackFeature
+            || feature === popupFeature
+            || super.hasFeature(feature);
+    }
+}
+
+export type SIssueSeverity = 'error' | 'warning' | 'info';
+
+export class SIssueMarker extends SDecoration {
+    messages: string[];
+    severity: SIssueSeverity;
+}
+
