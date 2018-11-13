00e66e3e8f736d421144397500392392f3188c77
Jan Koehnlein <jan.koehnlein@typefox.io>
2018-11-13 11:19:53 +0100

    [decorations] adapted issue markers to latest changes in sprotty-server (#20)

--- src/features/decoration/views.tsx
+++ src/features/decoration/views.tsx
@@ -26,23 +26,31 @@ export class IssueMarkerView implements IView {
     render(marker: SIssueMarker, context: RenderingContext): VNode {
         const scale = 16 / 1792;
         const trafo = `scale(${scale}, ${scale})`;
+        const maxSeverity = this.getMaxSeverity(marker);
         const group = <g class-sprotty-issue={true}>
             <g transform={trafo}>
-                <path d={this.getPath(marker.severity)} />
+                <path d={this.getPath(maxSeverity)} />
             </g>
         </g>;
-        setClass(group, 'sprotty-' + marker.severity, true);
+        setClass(group, 'sprotty-' + maxSeverity, true);
         return group;
+    }
+
+    protected getMaxSeverity(marker: SIssueMarker): SIssueSeverity {
+        let currentSeverity: SIssueSeverity = 'info';
+        for (const severity of marker.issues.map(s => s.severity)) {
+            if (severity === 'error' || (severity === 'warning' && currentSeverity === 'info'))
+                currentSeverity = severity;
+        }
+        return currentSeverity;
     }
 
     protected getPath(severity: SIssueSeverity) {
         switch (severity) {
             case 'error':
-                // tslint:disable-next-line:max-line-length
-                return "M768 128q209 0 385.5 103t279.5 279.5 103 385.5-103 385.5-279.5 279.5-385.5 103-385.5-103-279.5-279.5-103-385.5 103-385.5 279.5-279.5 385.5-103zm128 1247v-190q0-14-9-23.5t-22-9.5h-192q-13 0-23 10t-10 23v190q0 13 10 23t23 10h192q13 0 22-9.5t9-23.5zm-2-344l18-621q0-12-10-18-10-8-24-8h-220q-14 0-24 8-10 6-10 18l17 621q0 10 10 17.5t24 7.5h185q14 0 23.5-7.5t10.5-17.5z";
             case 'warning':
                 // tslint:disable-next-line:max-line-length
-                return "M1024 1375v-190q0-14-9.5-23.5t-22.5-9.5h-192q-13 0-22.5 9.5t-9.5 23.5v190q0 14 9.5 23.5t22.5 9.5h192q13 0 22.5-9.5t9.5-23.5zm-2-374l18-459q0-12-10-19-13-11-24-11h-220q-11 0-24 11-10 7-10 21l17 457q0 10 10 16.5t24 6.5h185q14 0 23.5-6.5t10.5-16.5zm-14-934l768 1408q35 63-2 126-17 29-46.5 46t-63.5 17h-1536q-34 0-63.5-17t-46.5-46q-37-63-2-126l768-1408q17-31 47-49t65-18 65 18 47 49z";
+                return "M768 128q209 0 385.5 103t279.5 279.5 103 385.5-103 385.5-279.5 279.5-385.5 103-385.5-103-279.5-279.5-103-385.5 103-385.5 279.5-279.5 385.5-103zm128 1247v-190q0-14-9-23.5t-22-9.5h-192q-13 0-23 10t-10 23v190q0 13 10 23t23 10h192q13 0 22-9.5t9-23.5zm-2-344l18-621q0-12-10-18-10-8-24-8h-220q-14 0-24 8-10 6-10 18l17 621q0 10 10 17.5t24 7.5h185q14 0 23.5-7.5t10.5-17.5z";
             case 'info':
                 // tslint:disable-next-line:max-line-length
                 return "M1024 1376v-160q0-14-9-23t-23-9h-96v-512q0-14-9-23t-23-9h-320q-14 0-23 9t-9 23v160q0 14 9 23t23 9h96v320h-96q-14 0-23 9t-9 23v160q0 14 9 23t23 9h448q14 0 23-9t9-23zm-128-896v-160q0-14-9-23t-23-9h-192q-14 0-23 9t-9 23v160q0 14 9 23t23 9h192q14 0 23-9t9-23zm640 416q0 209-103 385.5t-279.5 279.5-385.5 103-385.5-103-279.5-279.5-103-385.5 103-385.5 279.5-279.5 385.5-103 385.5 103 279.5 279.5 103 385.5z";
