772a8a38ade5906825be5ed5b18efc652538b4dc
tohosaku <tohosaku@users.osdn.me>
2021-05-30 06:06:23 +0900

    Upgrade eslint

--- configs/.eslintrc.js
+++ configs/.eslintrc.js
@@ -4,8 +4,7 @@ module.exports = {
         "node": true
     },
     "extends": [
-        "prettier",
-        "prettier/@typescript-eslint"
+        "prettier"
     ],
     "parser": "@typescript-eslint/parser",
     "parserOptions": {
