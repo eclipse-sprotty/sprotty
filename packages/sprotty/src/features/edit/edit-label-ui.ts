f4445dd8ca7102bdf677a221ddc7b47a426cdd7b
Martin Fleck <mfleck@eclipsesource.com>
2022-12-21 10:04:57 +0100

    Avoid sending label edit action if there is no change (#328)

--- packages/sprotty/src/features/edit/edit-label-ui.ts
+++ packages/sprotty/src/features/edit/edit-label-ui.ts
@@ -1,5 +1,5 @@
 /********************************************************************************
- * Copyright (c) 2019-2020 EclipseSource and others.
+ * Copyright (c) 2019-2022 EclipseSource and others.
  *
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License v. 2.0 which is available at
@@ -121,6 +121,11 @@ export class EditLabelUI extends AbstractUIExtension {
 
     protected async applyLabelEdit() {
         if (!this.isActive) {
+            return;
+        }
+        if (this.label?.text === this.editControl.value) {
+            // no action necessary
+            this.hide();
             return;
         }
         if (this.blockApplyEditOnInvalidInput) {
