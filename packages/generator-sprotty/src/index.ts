48ae42e5f011fdb0e303993b118be48ae6cc3aa9
Jonah Iden <jonah.iden@typefox.io>
2023-02-15 10:27:06 +0100

    review changes
    Signed-off-by: Jonah Iden <jonah.iden@typefox.io>

--- packages/generator-sprotty/src/index.ts
+++ packages/generator-sprotty/src/index.ts
@@ -1,3 +1,19 @@
+/********************************************************************************
+ * Copyright (c) 2023 TypeFox and others.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v. 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0.
+ *
+ * This Source Code may also be made available under the following Secondary
+ * Licenses when the conditions for such availability set forth in the Eclipse
+ * Public License v. 2.0 are satisfied: GNU General Public License, version 2
+ * with the GNU Classpath Exception which is available at
+ * https://www.gnu.org/software/classpath/license.html.
+ *
+ * SPDX-License-Identifier: EPL-2.0 OR GPL-2.0 WITH Classpath-exception-2.0
+ ********************************************************************************/
+
 import Generator from 'yeoman-generator';
 import chalk from 'chalk';
 import path from 'path';
@@ -69,8 +85,8 @@ class SprottyGenerator extends Generator {
                 name: 'projectName',
                 prefix: description(
                     'Welcome to Sprotty!',
-                    'This tool generates a new Sprotty Project',
-                    'The prject name is an identifier used in the extension marketplace or package registry.'
+                    'This tool generates a new Sprotty Project.\n',
+                    'The project name identifies the npm package and can be used by other packages to depend on this project.'
                 ),
                 message: 'Your project name:',
                 default: 'hello-world',
