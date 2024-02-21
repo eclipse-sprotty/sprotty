91fdf57d686783a993a3e312fec0b4054f5d1445
Guillaume Fontorbe <guillaume.fontorbe@typefox.io>
2024-02-21 13:42:08 +0100

    Add supports for shadow DOM
    Signed-off-by: Guillaume Fontorbe <guillaume.fontorbe@typefox.io>

--- packages/sprotty/src/base/views/viewer-options.ts
+++ packages/sprotty/src/base/views/viewer-options.ts
@@ -29,6 +29,8 @@ export interface ViewerOptions {
     /** CSS class added to the base element of the hidden rendering. */
     hiddenClass: string
     /** ID of the HTML element into which hover popup boxes are rendered. */
+    shadowRoot?: string
+    /** ID of the HTML element holding the shadow root. */
     popupDiv: string
     /** CSS class added to the base element of popup boxes. */
     popupClass: string
