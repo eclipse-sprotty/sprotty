15195a990160a5ae231ee7477a031c36752bbfb9
Guillaume Fontorbe <guillaume.fontorbe@typefox.io>
2024-04-30 12:30:29 +0200

    Fixed wrong behavior on update and set model
    Signed-off-by: Guillaume Fontorbe <guillaume.fontorbe@typefox.io>

--- packages/sprotty/src/features/edge-junction/junction-postprocessor.ts
+++ packages/sprotty/src/features/edge-junction/junction-postprocessor.ts
@@ -14,13 +14,14 @@
  * SPDX-License-Identifier: EPL-2.0 OR GPL-2.0 WITH Classpath-exception-2.0
  ********************************************************************************/
 
-import { injectable, inject } from "inversify";
-import { IVNodePostprocessor } from "../../base/views/vnode-postprocessor";
+import { inject, injectable } from "inversify";
 import { VNode } from "snabbdom";
-import { Action, RequestBoundsAction } from "sprotty-protocol";
+import { Action, RequestBoundsAction, SModelRoot } from "sprotty-protocol";
 import { SModelElementImpl } from "../../base/model/smodel";
 import { TYPES } from "../../base/types";
 import { ViewerOptions } from "../../base/views/viewer-options";
+import { IVNodePostprocessor } from "../../base/views/vnode-postprocessor";
+import { ModelSource } from "../../model-source/model-source";
 
 /**
  * Finds all junction points in the first SVG group element (diagram root level) and moves them to the end of the SVG.
@@ -28,38 +29,57 @@ import { ViewerOptions } from "../../base/views/viewer-options";
  */
 @injectable()
 export class JunctionPostProcessor implements IVNodePostprocessor {
+    currentModel: SModelRoot;
     isFirstRender = true;
 
     @inject(TYPES.ViewerOptions) private viewerOptions: ViewerOptions;
+    @inject(TYPES.ModelSource) private modelSource: ModelSource;
 
     decorate(vnode: VNode, element: SModelElementImpl): VNode {
         return vnode;
     }
     postUpdate(cause?: Action | undefined): void {
-        let targetDiv: string = this.viewerOptions.baseDiv;
-        if (cause?.kind === RequestBoundsAction.KIND) {
+
+        // if the model has changed, we need to remove the junction points from the previous model
+        if (this.currentModel !== this.modelSource.model) {
             this.isFirstRender = true;
-            targetDiv = this.viewerOptions.hiddenDiv;
         }
 
-        // remove moved junction points only if it is the first render cycle
-        if (this.isFirstRender) {
-            const outsideJunctionPoints = document.querySelectorAll(`#${targetDiv} > svg > g > g.sprotty-junction`);
-            outsideJunctionPoints.forEach(e => e.remove());
+        // if the cause of the update is a RequestBoundsAction (from the hidden model)
+        // and we are rendering the diagram for the first time (not from an update or setting the same model again)
+        // we need to remove the junction points from the previous model
+        if (cause?.kind === RequestBoundsAction.KIND && this.isFirstRender) {
+            const junctionPointsInHiddenDiv = document.querySelectorAll(`#${this.viewerOptions.hiddenDiv} > svg > g > g.sprotty-junction`);
+            junctionPointsInHiddenDiv.forEach(e => e.remove());
+
+            const junctionPointsInBaseDiv = document.querySelectorAll(`#${this.viewerOptions.baseDiv} > svg > g > g.sprotty-junction`);
+            junctionPointsInBaseDiv.forEach(e => e.remove());
         }
 
-        const junctionSelector = `#${targetDiv} > svg > g > g > g.sprotty-junction`;
-        const svg = document.querySelector(`#${targetDiv} > svg > g`);
-        if (svg) {
-            // find all nested junction points and move them down in the hierarchy so they are always rendered on top
-            const junctionGroups = Array.from(document.querySelectorAll(junctionSelector));
+        const hiddenSvg = document.querySelector(`#${this.viewerOptions.hiddenDiv} > svg > g`);
+        const baseSvg = document.querySelector(`#${this.viewerOptions.baseDiv} > svg > g`);
+
+        // move junction points to the end of the SVG in the hidden div
+        if (hiddenSvg) {
+            const junctionGroups = Array.from(document.querySelectorAll(`#${this.viewerOptions.hiddenDiv} > svg > g > g > g.sprotty-junction`));
             junctionGroups.forEach(junctionGroup => {
                 junctionGroup.remove();
             });
-            svg.append(...junctionGroups);
-
-            // if we are not rendering the hidden model, set the flag to false to prevent incorrect handling of junction points when updating/setting a new model
-            this.isFirstRender = targetDiv === this.viewerOptions.hiddenDiv;
+            hiddenSvg.append(...junctionGroups);
         }
+
+        // move junction points to the end of the SVG in the base div
+        if (baseSvg) {
+            const junctionGroups = Array.from(document.querySelectorAll(`#${this.viewerOptions.baseDiv} > svg > g > g > g.sprotty-junction`));
+            junctionGroups.forEach(junctionGroup => {
+                junctionGroup.remove();
+            });
+            baseSvg.append(...junctionGroups);
+        }
+
+        // update the current model
+        this.currentModel = this.modelSource.model;
+        // after the first render, we don't need to remove the junction points anymore
+        this.isFirstRender = false;
     }
 }
